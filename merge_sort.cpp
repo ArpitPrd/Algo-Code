273ac40b47f2f85864d8823662312da89733849f
ArpitPrd <prd.arpit@gmail.com>
2024-05-24 16:54:06 +0530

    merge-sort

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -1,4 +1,26 @@
 #include <iostream>
+#include <chrono>
+#include <cstdlib>
+#include <math.h>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+void plotData(const std::vector<int>& x, const std::vector<int>& y) {
+    std::ofstream dataFile("data.txt");
+    for (size_t i = 0; i < x.size(); ++i) {
+        dataFile << x[i] << " " << y[i] << "\n";
+    }
+    dataFile.close();
+
+    std::ofstream plotFile("plot.gnuplot");
+    plotFile << "set terminal png\n";
+    plotFile << "set output 'plot.png'\n";
+    plotFile << "plot 'data.txt' with lines\n";
+    plotFile.close();
+
+    system("gnuplot plot.gnuplot");
+}
 
 void PRINT_ARRAY(int* A, int len) {
 	for (int i = 0; i < len; i++) {
@@ -8,17 +30,17 @@ void PRINT_ARRAY(int* A, int len) {
 }
 
 void MERGE(int* A, int p, int q, int r) {
-	int L[q-p+2], R[r-q+1];
-	for (int i = 0; i < q-p+1; i++) {
+	int L[q - p + 1], R[r - q + 1];
+	for (int i = 0; i < q - p; i++) {
 		L[i] = A[i + p];
 	}
 	for (int i = 0; i < r-q; i++) {
-		R[i] = A[i + q + 1];
+		R[i] = A[i + q];
 	}
-	L[q-p+1] = 100000; // large number
-	R[r-q] = 100000;  // large number
+	L[q-p] = INT_MAX;
+	R[r-q] = INT_MAX;
 	int i = 0, j = 0;
-	for (int k = p; k < r; i++) {
+	for (int k = p; k < r; k++) {
 		if (L[i] <= R[j]) {
 			A[k] = L[i];
 			i += 1;
@@ -31,16 +53,32 @@ void MERGE(int* A, int p, int q, int r) {
 }
 
 void MERGE_SORT(int* A, int start, int end) {
-	if (start < end) {
+	if (end - start > 1) {
+        // cout << start <<  " " << end << endl;
 		MERGE_SORT(A, start, (start+end)/2);
-		MERGE_SORT(A, (start + end)/2 + 1, end);
+		MERGE_SORT(A, (start + end)/2, end);
 		MERGE(A, start, (start+end)/2, end);
 	}
 }
 
 int main() {
-	int* A = (int *) malloc(sizeof(int) * 5);
-	A[0] = 3; A[1] = 5; A[2] = 1; A[3] = 2; A[4] = 7;
-	MERGE_SORT(A, 0, 4);
-	PRINT_ARRAY(A, 5);
+    vector<int> v = {10, 100, 1000, 10000, 100000};
+    vector<int> time;
+    for (int l : v){
+        int len = l;
+    	cout << "length of array: " << len;
+    	int* A = (int *) malloc(sizeof(int) * len);
+    	cout << "unsorted array" << endl;
+        for (int i = 0; i < len; i++) {
+    		A[i] = rand() % 100;
+            cout << A[i] << " ";
+    	}
+        cout << endl;
+        auto start = std::chrono::high_resolution_clock::now();
+        MERGE_SORT(A, 0, len);
+        auto stop = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+        time.push_back(duration.count());
+    }
+    plotData(v, time);
 }
